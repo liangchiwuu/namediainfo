7a5f5c39d995703957dd8bcc231510d265b63c99
Liang-Chi Wuu <wuuhoom@gmail.com>
2018-04-01 01:49:09 -0700

    feat: refactor structure

--- namediainfo.py
+++ namediainfo.py
@@ -1,35 +1,40 @@
 import datetime
 import os
+import sys
 
 from pymediainfo import MediaInfo
 
-videos_dir = './videos/'
-hours_delta = -7
 
-
-def __parse_filename(utc_string):
-    naive = datetime.datetime.strptime(utc_string, "UTC %Y-%m-%d %H:%M:%S")
-    naive += datetime.timedelta(hours=hours_delta)
-    return naive.strftime("%Y%m%d_%H%M%S")
+def __parse_filename(utc_string, output_pattern, hour_offset):
+    naive = datetime.datetime.strptime(utc_string, 'UTC %Y-%m-%d %H:%M:%S')
+    naive += datetime.timedelta(hours=hour_offset)
+    return naive.strftime(output_pattern)
 
 
 def __rename(src, dst, retry=0):
     try:
-        if retry > 0:
-            dst = dst[:dst.rfind('.')] + '-{}'.format(retry) + dst[dst.rfind('.'):]
-        os.rename(src, dst)
-        print 'rename {} -> {}'.format(src, dst)
+        if retry == 0:
+            os.rename(src, dst)
+            print 'rename {} -> {}'.format(src, dst)
+        else:
+            dst_new = dst[:dst.rfind('.')] + '-{}'.format(retry) + dst[dst.rfind('.'):]
+            os.rename(src, dst_new)
+            print 'rename {} -> {}'.format(src, dst_new)
     except WindowsError:
         __rename(src, dst, retry + 1)
 
 
 def main():
-    for f in os.listdir(videos_dir):
-        media_info = MediaInfo.parse(videos_dir + f)
+    target_dir = sys.argv[1]
+    output_pattern = sys.argv[2]
+    hour_offset = float(sys.argv[3])
+
+    for f in os.listdir(target_dir):
+        media_info = MediaInfo.parse(os.path.join(target_dir, f))
         for track in media_info.tracks:
             if track.track_type == 'Video':
-                f_after = __parse_filename(track.encoded_date) + '.' + f.split('.')[-1]
-                __rename(videos_dir + f, videos_dir + f_after)
+                f_after = __parse_filename(track.encoded_date, output_pattern, hour_offset) + f[f.rfind('.'):]
+                __rename(os.path.join(target_dir, f), os.path.join(target_dir, f_after))
 
 
 if __name__ == '__main__':
